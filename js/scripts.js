1a4e893000e1aaeb7fe7cf3d1f9a8080d06b5a35
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-03-02 23:03:42 -0800

    added for loop for pokemon's name/height

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ let pokemonList = [
     {name: 'Squirtle',height: .5,types: 'Water'},
     {name: 'Jigglypuff',height: .5,types: ['Fairy', 'Normal']}
 ]
+//loop that reads and list off the name and height of the pokemons in pokemonList
+for (let i = 0; i < pokemonList.length; i++) {
+    //if pokemon is >= .6 then print special message 
+    document.write(` ${pokemonList[i].name} is ${pokemonList[i].height} meters tall. ${pokemonList[i].height >= .6 ? "That is a above average sized pokemon!" : ""}`)
+}

7893eb973408708a4653b9be4fc1b167d01a9311
ethangalva <82258388+ethangalva@users.noreply.github.com>
2022-03-05 09:17:16 -0800

    IIFE integration and basic functions to interact
    Interaction with the array pokemonList is now possible via 2 functions inside of the global variable pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
-//array of pokemon in the pokedex
-let pokemonList = [
+let pokemonRepository = (function () {
+    let pokemonList = [
     {name: 'Charmander',height: .6,types: 'Fire'},
     {name: 'Squirtle',height: .5,types: 'Water'},
     {name: 'Jigglypuff',height: .5,types: ['Fairy', 'Normal']}
-]
-//loop that reads and list off the name and height of the pokemons in pokemonList
+    ]
 
+    //returns all information of pokemons
+    function getAll() {
+        return pokemonList;
+    }
+    //pushes new pokemons onto the array
+    function add(item) {
+        pokemonList.push(item);
+    }
 
-pokemonList.forEach( item => console.log(item));
+    //returns the values of the functions for them to be called outside of the IIFE
+    return {
+        add: add,
+        getAll: getAll
+    }
+
+})();
+//Print pokemons information in main body of html
+pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(pokemon.name + " height: "+ pokemon.height + ", " + "<br/>");
+  });
+//Add pokemon to the end of the array
+console.log(pokemonRepository.getAll());
+pokemonRepository.add("Ethan");
+console.log(pokemonRepository.getAll());
